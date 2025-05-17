9e54a1cd44585f47b56887c8a67b043e032d28e0
IljaN <node512@gmail.com>
2025-05-17 02:25:27 +0200

    Use gliderlabs/ssh and own Server struct

--- services/sftp/pkg/command/server.go
+++ services/sftp/pkg/command/server.go
@@ -3,33 +3,15 @@ package command
 import (
 	"context"
 	"fmt"
-
-	gatewayv1beta1 "github.com/cs3org/go-cs3apis/cs3/gateway/v1beta1"
-	userpb "github.com/cs3org/go-cs3apis/cs3/identity/user/v1beta1"
-	rpc "github.com/cs3org/go-cs3apis/cs3/rpc/v1beta1"
-	providerv1beta1 "github.com/cs3org/go-cs3apis/cs3/storage/provider/v1beta1"
-
-	"google.golang.org/grpc/metadata"
-
-	ctxpkg "github.com/opencloud-eu/reva/v2/pkg/ctx"
-
-	"github.com/opencloud-eu/opencloud/pkg/registry"
-
-	"github.com/opencloud-eu/reva/v2/pkg/rgrpc/todo/pool"
-	"github.com/opencloud-eu/reva/v2/pkg/sharedconf"
-	"io"
-	"net"
-	"os"
+	"github.com/opencloud-eu/opencloud/services/sftp/pkg/server"
 
 	"github.com/oklog/run"
 	"github.com/opencloud-eu/opencloud/pkg/config/configlog"
-	"github.com/opencloud-eu/opencloud/pkg/log"
 	"github.com/opencloud-eu/opencloud/services/sftp/pkg/config"
 	"github.com/opencloud-eu/opencloud/services/sftp/pkg/config/parser"
 	"github.com/opencloud-eu/opencloud/services/sftp/pkg/logging"
-	"github.com/pkg/sftp"
+	"github.com/opencloud-eu/reva/v2/pkg/sharedconf"
 	"github.com/urfave/cli/v2"
-	"golang.org/x/crypto/ssh"
 )
 
 // Server is the entry point for the server command.
@@ -45,7 +27,7 @@ func Server(cfg *config.Config) *cli.Command {
 			logger := logging.Configure(cfg.Service.Name, cfg.Log)
 
 			gr := run.Group{}
-			ctx, cancel := context.WithCancel(c.Context)
+			_, cancel := context.WithCancel(c.Context)
 
 			defer cancel()
 
@@ -64,8 +46,11 @@ func Server(cfg *config.Config) *cli.Command {
 				}
 
 				cfg.GatewaySelector = "eu.opencloud.api.gateway"
-
-				sftpSrv(ctx, cfg, logger)
+				srv := server.NewSFTPServer(cfg, logger)
+				err := srv.ListenAndServe()
+				if err != nil {
+					return err
+				}
 
 				return nil
 
@@ -87,163 +72,5 @@ func Server(cfg *config.Config) *cli.Command {
 
 			return gr.Run()
 		},
-	}
-}
-
-func sftpSrv(ctx context.Context, cfg *config.Config, lg log.Logger) {
-	debugStream := io.Discard
-	if false {
-		debugStream = os.Stderr
-	}
-
-	gatewaySelector, err := pool.GatewaySelector(
-		cfg.Reva.Address,
-		append(
-			cfg.Reva.GetRevaOptions(),
-			pool.WithRegistry(registry.GetRegistry()),
-		)...)
-	if err != nil {
-		panic(err)
-	}
-
-	gwapi, err := gatewaySelector.Next()
-	if err != nil {
-		panic(err)
-	}
-
-	// An SSH server is represented by a ServerConfig, which holds
-	// certificate details and handles authentication of ServerConns.
-	config := &ssh.ServerConfig{
-
-		PasswordCallback: func(c ssh.ConnMetadata, pass []byte) (*ssh.Permissions, error) {
-
-			fmt.Fprintf(debugStream, "Login: %s\n", c.User())
-			userName := c.User()
-
-			ctx := context.Background()
-			authRes, err := gwapi.Authenticate(ctx, &gatewayv1beta1.AuthenticateRequest{
-				Type:         "machine",
-				ClientId:     "username:" + userName,
-				ClientSecret: cfg.MachineAuthAPIKey,
-			})
-
-			if err != nil {
-				lg.Debug().Err(err).Msgf("Auth failed")
-				return nil, err
-			}
-
-			if authRes.GetStatus().GetCode() != rpc.Code_CODE_OK {
-				authErr := fmt.Errorf(authRes.GetStatus().GetMessage())
-				lg.Debug().Err(authErr).Msgf("Auth request returned Not OK")
-				return nil, authErr
-			}
-
-			granteeCtx := ctxpkg.ContextSetUser(context.Background(), &userpb.User{Id: authRes.GetUser().GetId()})
-			granteeCtx = metadata.AppendToOutgoingContext(granteeCtx, ctxpkg.TokenHeader, authRes.GetToken())
-
-			// Get Home Path
-			homeResp, err := gwapi.GetHome(granteeCtx, &providerv1beta1.GetHomeRequest{})
-			if err != nil {
-				panic(err)
-			}
-
-			// List Spaces
-			storageSpacesList, err := gwapi.ListStorageSpaces(granteeCtx, &providerv1beta1.ListStorageSpacesRequest{
-				PageSize:  0,
-				PageToken: "",
-			})
-
-			if err != nil {
-				panic(err)
-			}
-
-			fmt.Println(homeResp.GetPath())
-			fmt.Println(storageSpacesList.String())
-
-			return nil, fmt.Errorf("password rejected for %q", c.User())
-		},
-	}
-
-	privateBytes, err := os.ReadFile(cfg.ServerCertPath)
-	if err != nil {
-		lg.Fatal().Err(err).Msg("Failed to load private key")
-	}
-
-	private, err := ssh.ParsePrivateKey(privateBytes)
-	if err != nil {
-		lg.Fatal().Err(err).Msg("Failed to parse private key")
-	}
-
-	config.AddHostKey(private)
-
-	// Once a ServerConfig has been configured, connections can be
-	// accepted.
-	listener, err := net.Listen("tcp", "127.0.0.1:2022")
-	if err != nil {
-		lg.Fatal().Err(err).Msg("failed to listen for connection")
-	}
-	fmt.Printf("Listening on %v\n", listener.Addr())
-
-	nConn, err := listener.Accept()
-	if err != nil {
-		lg.Fatal().Err(err).Msg("failed to accept incoming connection")
-	}
-
-	// Before use, a handshake must be performed on the incoming net.Conn.
-	sconn, chans, reqs, err := ssh.NewServerConn(nConn, config)
-	if err != nil {
-		lg.Fatal().Err(err).Msg("failed to handshake")
-	}
-	lg.Info().Msgf("login detected: %s", sconn.User())
-	fmt.Fprintf(debugStream, "SSH server established\n")
-
-	// The incoming Request channel must be serviced.
-	go ssh.DiscardRequests(reqs)
-
-	// Service the incoming Channel channel.
-	for newChannel := range chans {
-		// Channels have a type, depending on the application level
-		// protocol intended. In the case of an SFTP session, this is "subsystem"
-		// with a payload string of "<length=4>sftp"
-		fmt.Fprintf(debugStream, "Incoming channel: %s\n", newChannel.ChannelType())
-		if newChannel.ChannelType() != "session" {
-			newChannel.Reject(ssh.UnknownChannelType, "unknown channel type")
-			fmt.Fprintf(debugStream, "Unknown channel type: %s\n", newChannel.ChannelType())
-			continue
-		}
-		channel, requests, err := newChannel.Accept()
-		if err != nil {
-			lg.Fatal().Err(err).Msg("could not accept channel.")
-		}
-		fmt.Fprintf(debugStream, "Channel accepted\n")
-
-		// Sessions have out-of-band requests such as "shell",
-		// "pty-req" and "env".  Here we handle only the
-		// "subsystem" request.
-		go func(in <-chan *ssh.Request) {
-			for req := range in {
-				fmt.Fprintf(debugStream, "Request: %v\n", req.Type)
-				ok := false
-				switch req.Type {
-				case "subsystem":
-					fmt.Fprintf(debugStream, "Subsystem: %s\n", req.Payload[4:])
-					if string(req.Payload[4:]) == "sftp" {
-						ok = true
-					}
-				}
-				fmt.Fprintf(debugStream, " - accepted: %v\n", ok)
-				req.Reply(ok, nil)
-			}
-		}(requests)
-
-		root := sftp.InMemHandler()
-		server := sftp.NewRequestServer(channel, root)
-		if err := server.Serve(); err != nil {
-			if err != io.EOF {
-				lg.Fatal().Err(err).Msg("sftp server completed with error:")
-			}
-		}
-		server.Close()
-		lg.Info().Msg("sftp client exited session.")
 	}
 }
---
+++ services/sftp/pkg/server/sftp.go
@@ -0,0 +1,154 @@
+package server
+
+import (
+	"context"
+	"fmt"
+	gateway "github.com/cs3org/go-cs3apis/cs3/gateway/v1beta1"
+	gatewayv1beta1 "github.com/cs3org/go-cs3apis/cs3/gateway/v1beta1"
+	userpb "github.com/cs3org/go-cs3apis/cs3/identity/user/v1beta1"
+	rpc "github.com/cs3org/go-cs3apis/cs3/rpc/v1beta1"
+	providerv1beta1 "github.com/cs3org/go-cs3apis/cs3/storage/provider/v1beta1"
+	"github.com/gliderlabs/ssh"
+	"github.com/opencloud-eu/opencloud/pkg/log"
+	"github.com/opencloud-eu/opencloud/pkg/registry"
+	sftpSvrCfg "github.com/opencloud-eu/opencloud/services/sftp/pkg/config"
+	ctxpkg "github.com/opencloud-eu/reva/v2/pkg/ctx"
+	"github.com/opencloud-eu/reva/v2/pkg/rgrpc/todo/pool"
+	"github.com/pkg/sftp"
+	gossh "golang.org/x/crypto/ssh"
+	"google.golang.org/grpc/metadata"
+	"io"
+	"os"
+)
+
+type SFTPServer struct {
+	*ssh.Server
+
+	gwSelector *pool.Selector[gateway.GatewayAPIClient]
+	cfg        *sftpSvrCfg.Config
+	log        log.Logger
+}
+
+func NewSFTPServer(cfg *sftpSvrCfg.Config, logger log.Logger) *SFTPServer {
+	s := &SFTPServer{
+		Server: &ssh.Server{
+			Addr: "127.0.0.1:2222",
+		},
+		cfg: cfg,
+		log: logger,
+	}
+
+	s.SubsystemHandlers = map[string]ssh.SubsystemHandler{
+		"sftp": s.SFTPHandler,
+	}
+
+	s.PasswordHandler = s.HandleAuth
+
+	return s
+}
+
+// SFTPHandler handler for SFTP subsystem
+func (s *SFTPServer) SFTPHandler(sess ssh.Session) {
+	server := sftp.NewRequestServer(
+		sess,
+		sftp.InMemHandler(),
+	)
+
+	gwapi, err := s.gwSelector.Next()
+	if err != nil {
+		s.log.Err(err).Msg("Failed to get gateway client")
+	}
+
+	uid, ok := sess.Context().Value("uid").(*userpb.UserId)
+	if !ok {
+		s.log.Err(err).Msg("Failed to get uid from ctx")
+		return
+	}
+
+	token, ok := sess.Context().Value("token").(string)
+	if !ok {
+		s.log.Err(err).Msg("Failed to get token from ctx")
+		return
+	}
+
+	granteeCtx := ctxpkg.ContextSetUser(context.Background(), &userpb.User{Id: uid})
+	granteeCtx = metadata.AppendToOutgoingContext(granteeCtx, ctxpkg.TokenHeader, token)
+
+	// Get Home Path
+	homeResp, err := gwapi.GetHome(granteeCtx, &providerv1beta1.GetHomeRequest{})
+	if err != nil {
+		panic(err)
+	}
+
+	fmt.Println(homeResp.String())
+
+	if err := server.Serve(); err == io.EOF {
+		server.Close()
+		fmt.Println("sftp client exited session.")
+	} else if err != nil {
+		fmt.Println("sftp server completed with error:", err)
+	}
+}
+
+func (s *SFTPServer) HandleAuth(ctx ssh.Context, passwd string) bool {
+	gwapi, err := s.gwSelector.Next()
+	if err != nil {
+		return false
+	}
+
+	userName := ctx.User()
+
+	//ctx := context.Background()
+	authRes, err := gwapi.Authenticate(ctx, &gatewayv1beta1.AuthenticateRequest{
+		Type:         "machine",
+		ClientId:     "username:" + userName,
+		ClientSecret: s.cfg.MachineAuthAPIKey,
+	})
+
+	if err != nil {
+		s.log.Debug().Err(err).Msgf("Auth failed")
+		return false
+	}
+
+	if authRes.GetStatus().GetCode() == rpc.Code_CODE_OK {
+		ctx.SetValue("uid", authRes.GetUser().GetId())
+		ctx.SetValue("token", authRes.GetToken())
+		return true
+	}
+
+	authErr := fmt.Errorf(authRes.GetStatus().GetMessage())
+	s.log.Debug().Err(authErr).Msgf("Auth request returned rejected for %s", userName)
+	return false
+}
+
+func (s *SFTPServer) ListenAndServe() error {
+	key, err := readPrivateKeyFromFile(s.cfg.ServerCertPath)
+	if err != nil {
+		return err
+	}
+
+	s.AddHostKey(key)
+
+	sel, err := pool.GatewaySelector(
+		s.cfg.Reva.Address,
+		append(
+			s.cfg.Reva.GetRevaOptions(),
+			pool.WithRegistry(registry.GetRegistry()),
+		)...)
+	if err != nil {
+		return err
+	}
+
+	s.gwSelector = sel
+
+	return s.Server.ListenAndServe()
+}
+
+func readPrivateKeyFromFile(certPath string) (gossh.Signer, error) {
+	privateBytes, err := os.ReadFile(certPath)
+	if err != nil {
+		return nil, err
+	}
+
+	return gossh.ParsePrivateKey(privateBytes)
+}
